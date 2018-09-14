99425d39e946188cceab9885252b4958248b753f
pk100101 <42978490+pk100101@users.noreply.github.com>
2018-09-13 23:08:48 -0400

    Add files via upload
    Variable HW 9/13

---
+++ src/hw/VariableHomework.java
@@ -0,0 +1,29 @@
+package hw;
+
+/**
+ * 
+ * @author Pragnya
+ *
+ */
+public class VariableHomework {
+
+	public static void main(String[] args) {
+	
+		int height = 5;
+		int width = 7;
+		int area = height * width;
+		System.out.print(area);
+	
+		double pi = 3.14;
+		int radius = 4;
+		double areaCircle = pi * radius *radius;
+		double circumference = 2 * pi * radius;
+		System.out.println(areaCircle);
+		System.out.println(circumference);
+	
+		String name = "SwaggyP";
+		System.out.println(name);
+		
+	}
+
+}
